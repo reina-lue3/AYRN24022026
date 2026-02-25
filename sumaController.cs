a8a6ef2ed13a78f57b59cb90c41be60571ba82fd
abigail23181 <abigailrodrigxz@gmail.com>
2026-02-24 22:28:24 -0600

    Add files via upload

---
+++ sumaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ARNY240498.AppMVC.Controllers
+{
+    public class sumaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        // Acción para procesar la suma
+        [HttpPost]
+        public IActionResult Index(double num1, double num2)
+        {
+            double resultado = num1 + num2;
+            ViewBag.Resultado = resultado;
+            ViewBag.Num1 = num1;
+            ViewBag.Num2 = num2;
+
+            return View();
+        }
+    }
+}
