6f38ed3713825440d442fc71bff216ddd6fadb92
MINEDUCYT <MINEDUCYT@REINA>
2026-02-24 18:49:09 -0600

    Agregar archivos de proyecto.

---
+++ AYRN24022026.AppMVC/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using AYRN24022026.AppMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AYRN24022026.AppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AYRN24022026.AppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AYRN24022026.AppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
